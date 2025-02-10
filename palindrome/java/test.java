a265be9402800354165165059662d9c2c2f90be1
Masaya Sato <masaput1282@yahoo.co.jp>
2025-02-10 10:05:00 +0900

    Upload a project

---
+++ palindrome/java/test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Solution.isPalindrome("kayak")); // true
+        System.out.println(Solution.isPalindrome("recursion")); // false
+        System.out.println(Solution.isPalindrome("Top spot")); // true
+        System.out.println(Solution.isPalindrome("Was it a cat I saw")); // true
+        System.out.println(Solution.isPalindrome("ad")); // false
+    }
+}
