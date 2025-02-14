436b191432d8fc62aae8cf948302bad4a44f9d0e
Masaya Sato <masaput1282@yahoo.co.jp>
2025-02-14 19:19:25 +0900

    solution for php

---
+++ HigherOrderFunc/compound/php/test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'solution.php';
+
+var_dump(calculateFinalMoney([3,5,2,5,4],100));
+var_dump(calculateFinalMoney([5,5,4,6,7],2000));
+var_dump(calculateFinalMoney([2,3,3,3,4],40000));
