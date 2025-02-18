f857b4c28844ed8ab4846c6b48a7640fa2b080bd
Masaya Sato <masaput1282@yahoo.co.jp>
2025-02-19 07:26:11 +0900

    Add a new question 'decimalToBinary'

---
+++ ControlFlow/decimalToBinary/javascript/test.js
@@ -0,0 +1,7 @@
+const decimalToBinary = require('./solution.js');
+
+console.log(decimalToBinary(60)); 
+console.log(decimalToBinary(26)); 
+console.log(decimalToBinary(35)); 
+console.log(decimalToBinary(100)); 
+console.log(decimalToBinary(505)); 
---
+++ ControlFlow/decimalToBinary/php/test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'solution.php';
+
+echo decimalToBinary(60) . PHP_EOL; 
+echo decimalToBinary(26) . PHP_EOL; 
+echo decimalToBinary(35) . PHP_EOL; 
+echo decimalToBinary(100) . PHP_EOL; 
+echo decimalToBinary(505) . PHP_EOL; 
