6347b5b07af72bcbea1e89dc3c5887cbeaf27feb
Masaya Sato <masaput1282@yahoo.co.jp>
2025-02-20 07:18:22 +0900

    Add a new question 'gcd'

---
+++ ControlFlow/gcd/php/test.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once './solution.php';
+
+var_dump(gcd(44,242));
+var_dump(gcdWhileLoopIteration(3355,2379));
+var_dump(gcdWhileLoopIteration(107, 31));
+var_dump(gcdWhileLoopIterationReturn(44,242));
+var_dump(gcdWhileLoopIterationReturn(0, 55));
+var_dump(gcdWhileLoopIterationFlag(44,242));
+var_dump(gcdWhileLoopIterationFlag(48, 48));
