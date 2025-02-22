9ec3343e2f2a5ae486646aa2df35b6b9dbbb14e7
Masaya Sato <masaput1282@yahoo.co.jp>
2025-02-23 07:36:09 +0900

    Add a new question 'perfectNumber'

---
+++ ControlFlow/perfectNumber/javascript/test.js
@@ -0,0 +1,9 @@
+const perfectNumberList = require('./solution');
+
+console.log(perfectNumberList(3));
+console.log(perfectNumberList(6));
+console.log(perfectNumberList(28));
+console.log(perfectNumberList(100));
+console.log(perfectNumberList(496));
+console.log(perfectNumberList(1000));
+console.log(perfectNumberList(10000));
