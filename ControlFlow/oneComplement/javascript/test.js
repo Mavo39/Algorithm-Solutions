482bc31676f40e57b3fbd67b5d2217ad93afb583
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-02-25 11:34:06 +0900

    Add a new question 'oneComplement'

---
+++ ControlFlow/oneComplement/javascript/test.js
@@ -0,0 +1,7 @@
+const oneComplement = require('./solution');
+
+console.log(oneComplement("00011100"));
+console.log(oneComplement("10010"));
+console.log(oneComplement("001001"));
+console.log(oneComplement("0111010"));
+console.log(oneComplement("1"));
---
+++ ControlFlow/oneComplement/php/test.php
@@ -0,0 +1,8 @@
+<?php
+require_once './solution.php';
+
+var_dump(oneComplement("00011100"));
+var_dump(oneComplement("10010"));
+var_dump(oneComplement("001001"));
+var_dump(oneComplement("0111010"));
+var_dump(oneComplement("1"));
