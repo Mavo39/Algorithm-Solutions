fbe1c5d86494cd66d37884ae43c82a6b947d8d46
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-03-16 12:09:47 +0900

    Add a new question 'oddSum' and solve it on JavaScript

---
+++ List/oddSum/javascript/test.js
@@ -0,0 +1,5 @@
+const sumOfOddElement = require('./solution.js');
+
+console.log(sumOfOddElement([2,4,3,8,11]));
+console.log(sumOfOddElement([1]));
+console.log(sumOfOddElement([21,4,436,85,182,169]));
