22fedbbad04a3f592b6583be5a88e7c0847dae43
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-04-05 09:35:31 +0900

    「リスト内の要素の足し合わせ」

---
+++ List/addEveryOtherElement/js/test.js
@@ -0,0 +1,7 @@
+const addEveryOtherElement = require('./solution');
+
+console.log(addEveryOtherElement([34,46,45,57]));
+console.log(addEveryOtherElement([60,45,66,75,56,32,654,45,100]));
+console.log(addEveryOtherElement([5,10]));
+console.log(addEveryOtherElement([120,85,45,67,54,343,57,800,88,67,56,57,68,75]));
+console.log(addEveryOtherElement([]));
