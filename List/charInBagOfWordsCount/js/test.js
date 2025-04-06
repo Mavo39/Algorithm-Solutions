01ab207df59f8f3275f793214fbcc6ef2a19bfbb
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-04-06 11:57:34 +0900

    「単語内の文字カウント」を追加

---
+++ List/charInBagOfWordsCount/js/test.js
@@ -0,0 +1,6 @@
+const charInBagOfWordsCount = require('./solution');
+
+console.log(charInBagOfWordsCount(["hello","world"],'o'));
+console.log(charInBagOfWordsCount(["hello","world"],'p'));
+console.log(charInBagOfWordsCount(["The","tech","giant","is in the","city center"],'p'));
+console.log(charInBagOfWordsCount(["The","tech","giant","is in the","city center"],'e'));
