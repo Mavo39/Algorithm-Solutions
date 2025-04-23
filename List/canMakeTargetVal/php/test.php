74cd1bc130a8c2249d1fdfd99900c6010eb171fe
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-04-23 17:50:28 +0900

    「合計値が等しい」を追加

---
+++ List/canMakeTargetVal/php/test.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once './solution.php';
+
+var_dump(canMakeTargetVal([3,4,2,5,1],15)) . PHP_EOL;
+var_dump(canMakeTargetVal([4,11,23,9,6],20)) . PHP_EOL;
+var_dump(canMakeTargetVal([23,14,65,71,2],2)) . PHP_EOL;
+var_dump(canMakeTargetVal([2,1,3,4],2)) . PHP_EOL;
