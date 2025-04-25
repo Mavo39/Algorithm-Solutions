41ff5c1fcd2c77218cf722a9e0e9d7e5dede3981
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-04-25 21:30:22 +0900

    「キャラクター操作」を追加

---
+++ List/characterLocation/php/test.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once './solution.php';
+
+echo json_encode(characterLocation("NNNN")) . PHP_EOL;
+echo json_encode(characterLocation("NESW")) . PHP_EOL;
+echo json_encode(characterLocation("NW")) . PHP_EOL;
+echo json_encode(characterLocation("AWFGMD")) . PHP_EOL;
+echo json_encode(characterLocation("NSSNWENSSNNN")) . PHP_EOL;
