11477b56e34de2d4e826a9fc2ce7961e644a2a3c
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-05-07 21:49:24 +0900

    「足して合計値」を追加・回答途中

---
+++ List/twoSum/php/test.php
@@ -0,0 +1,15 @@
+<?php 
+
+require_once './solution.php';
+
+echo json_encode(twoSum([1,2,3,4],5)) . PHP_EOL;
+echo json_encode(twoSum([1,1],2)) . PHP_EOL;
+echo json_encode(twoSum([1,1],5)) . PHP_EOL;
+echo json_encode(twoSum([1,2,3],4)) . PHP_EOL;
+echo json_encode(twoSum([3,10,11,15],14)) . PHP_EOL;
+echo json_encode(twoSum([375,406,515,406,207,293,923,94,862,329,767,785,364],1114)) . PHP_EOL;
+echo json_encode(twoSum([574,11,955,912,933,105,993,61,678,4,618,916,866,25,313,840,699,653,141,748,464,816,512,461,123],1664)) . PHP_EOL;
+echo json_encode(twoSum([444,73,978,616,585,298,548,632,390,970,822,839,71,541,362,697,134,580,692,227,191,395],1136)) . PHP_EOL;
+echo json_encode(twoSum([468,883,524,54,892,447,326,536,189,214,450],540)) . PHP_EOL;
+echo json_encode(twoSum([747,34,628,747,214,334,808,866,71,547],1022)) . PHP_EOL;
+echo json_encode(twoSum([214,666,347,904,817,209,365,563,479,231,58,162,324,40,632,267,949,126,863,749,773,705,217,903],1766)) . PHP_EOL;
