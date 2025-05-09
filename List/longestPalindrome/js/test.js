8cfef8b41ae29bafe73d12c8bf11af0e160e93cd
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-05-09 22:21:56 +0900

    「クイズゲーム」を追加（回答途中）

---
+++ List/longestPalindrome/js/test.js
@@ -0,0 +1,14 @@
+const longestPalindromeLength = require('./solution');
+
+console.log(longestPalindromeLength("abcccdd"));
+console.log(longestPalindromeLength("kayak"));
+console.log(longestPalindromeLength("abcdefa"));
+console.log(longestPalindromeLength("abcdabct"));
+console.log(longestPalindromeLength("abcdefg"));
+console.log(longestPalindromeLength("hello"));
+console.log(longestPalindromeLength("forkayakkayakfor"));
+console.log(longestPalindromeLength("nursesrunatthehole"));
+console.log(longestPalindromeLength("sitnumquametaspernatur."));
+console.log(longestPalindromeLength("harumautdoloresetaperiamrationeestvoluptatesexercitationem."));
+console.log(longestPalindromeLength("quidebitisaspernaturvelfugautexpeditaassumenda."));
+console.log(longestPalindromeLength("omnisetidest."));
