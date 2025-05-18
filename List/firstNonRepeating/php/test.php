78aa61b47520d5de3fccf65b891b6bd15b6ef7d0
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-05-18 10:19:51 +0900

    「復習のお知らせ機能」を追加

---
+++ List/firstNonRepeating/php/test.php
@@ -0,0 +1,11 @@
+<?php 
+
+require_once './solution.php';
+
+var_dump(firstNonRepeating("aabbcdddeffg")) . PHP_EOL;
+var_dump(firstNonRepeating("abcdabcdf")) . PHP_EOL;
+var_dump(firstNonRepeating("abcddaebcdf")) . PHP_EOL;
+var_dump(firstNonRepeating("aabbbccdd")) . PHP_EOL;
+var_dump(firstNonRepeating("ddecdfgf")) . PHP_EOL;
+var_dump(firstNonRepeating("abcdeeff")) . PHP_EOL;
+var_dump(firstNonRepeating("zzcbdefghafhgbbcd")) . PHP_EOL;
