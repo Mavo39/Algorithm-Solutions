a1a12f8724bc9fe18445397dd3fd6a7eb7134007
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-02 20:55:41 +0900

    「ソート済み連結リストの合併」回答中

---
+++ LinkedList/mergeTwoSortedLinkedLists/php/test.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once '../../Common/php/Node.php';
+require_once '../../Common/php/SinglyLinkedList.php';
+
+$list1 = new SinglyLinkedList([1,2,3]);
+$list2 = new SinglyLinkedList([1,4,5]);
+
