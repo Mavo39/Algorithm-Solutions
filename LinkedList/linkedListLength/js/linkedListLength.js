2d82e9793ab1b35c6ae1976ebf3dc5b92fab03b8
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-07 17:54:52 +0900

    Add edge case in linkedListLength

--- LinkedList/linkedListLength/js/linkedListLength.js
+++ LinkedList/linkedListLength/js/linkedListLength.js
@@ -1,6 +1,8 @@
 import { SinglyLinkedList } from '../../Common/js/SinglyLinkedList.js';
 
 export function linkedListLength(head){
+    if(head === null) return 0;
+
     let count = 0;
     let currentNode = head;
 
