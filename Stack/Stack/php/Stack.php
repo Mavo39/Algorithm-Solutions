3e4d91e90cf037475dc0944509049324ce6c4edb
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-17 20:48:16 +0900

    implement Stack class with push, pop, and peek methods

---
+++ Stack/Stack/php/Stack.php
@@ -0,0 +1,32 @@
+<?php 
+
+require_once './Item.php';
+
+class Stack {
+    public $head;
+
+    public function __construct(){
+        $this->head = null;
+    }
+
+    public function push($data){
+        $temp = $this->head;
+        $this->head = new Item($data);
+        $this->head->next = $temp;
+    }
+
+    public function pop(){
+        if($this->head === null) return null;
+
+        $temp = $this->head;
+        $this->head = $this->head->next;
+
+        return $temp->data;
+    }
+
+    public function peek(){
+        if($this->head === null) return null;
+
+        return $this->head->data;
+    }
+}
