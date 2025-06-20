8f783aa54124088bec1b4695f3b497ebdffc9839
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-20 20:52:54 +0900

    Improve performance by replacing unshift with direct index assignment

--- Stack/dailyStockPrice/js/dailyStockPrice.js
+++ Stack/dailyStockPrice/js/dailyStockPrice.js
@@ -1,26 +1,26 @@
-export function dailyStockPrice(stocks){
-    let stack = [];
-    let resArr = [];
+// リファクタリング後のコード
+export function dailyStockPrice(stocks) {
+    const n = stocks.length;
+    const resArr = new Array(n).fill(0);
+    const stack = [];
 
-    for (let i = stocks.length - 1; i >= 0; i--) {
+    for (let i = n - 1; i >= 0; i--) {
         const currentPrice = stocks[i];
 
         while (stack.length > 0) {
-            const stackTopIndex = stack[stack.length - 1];
-            const stackTopPrice = stocks[stackTopIndex];
+            const topIndex = stack[stack.length - 1];
+            const topPrice = stocks[topIndex];
 
-            if (stackTopPrice <= currentPrice) {
+            if (topPrice <= currentPrice) {
                 stack.pop();
             } else {
                 break;
             }
         }
 
-        if (stack.length === 0) {
-            resArr.unshift(0);
-        } else {
-            const stackTopIndex = stack[stack.length - 1];
-            resArr.unshift(stackTopIndex - i);
+        if (stack.length > 0) {
+            // stack[stack.length - 1]: スタックの末尾
+            resArr[i] = stack[stack.length - 1] - i;
         }
 
         stack.push(i);
@@ -28,3 +28,35 @@ export function dailyStockPrice(stocks){
 
     return resArr;
 }
+
+// 自分が最初に書いたコード
+// export function dailyStockPrice(stocks){
+//     let stack = [];
+//     let resArr = [];
+
+//     for (let i = stocks.length - 1; i >= 0; i--) {
+//         const currentPrice = stocks[i];
+
+//         while (stack.length > 0) {
+//             const stackTopIndex = stack[stack.length - 1];
+//             const stackTopPrice = stocks[stackTopIndex];
+
+//             if (stackTopPrice <= currentPrice) {
+//                 stack.pop();
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         if (stack.length === 0) {
+//             resArr.unshift(0);
+//         } else {
+//             const stackTopIndex = stack[stack.length - 1];
+//             resArr.unshift(stackTopIndex - i);
+//         }
+
+//         stack.push(i);
+//     }
+
+//     return resArr;
+// }
