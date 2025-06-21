d9b8ef356de023f65a45615477622817a84b9dac
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-21 15:18:19 +0900

    refactor: extract common logic into testPalindromeLinkedList for cleaner test cases

--- Stack/palindromeLinkedList/php/test.php
+++ Stack/palindromeLinkedList/php/test.php
@@ -5,30 +5,28 @@ require_once __DIR__ . '/palindromeLinkedList.php';
 
 use Common\SinglyLinkedList;
 
+function testPalindromeLinkedList(array $values): string {
+    $list = new SinglyLinkedList($values);
+    return palindromeLinkedList($list->head) ? "true" : "false";
+}
+
 // 1.
-$list1 = new SinglyLinkedList([1,2,3]);
-echo palindromeLinkedList($list1->head) . PHP_EOL;
+echo testPalindromeLinkedList([1,2,3]) . PHP_EOL;
 
 // 2.
-$list2 = new SinglyLinkedList([1,2]);
-echo palindromeLinkedList($list2->head) . PHP_EOL;
+echo testPalindromeLinkedList([1,2]) . PHP_EOL;
 
 // 3.
-$list3 = new SinglyLinkedList([1,0,1]);
-echo palindromeLinkedList($list3->head) . PHP_EOL;
+echo testPalindromeLinkedList([1,0,1]) . PHP_EOL;
 
 // 4.
-$list4 = new SinglyLinkedList([3,4,4,3,6]);
-echo palindromeLinkedList($list4->head) . PHP_EOL;
+echo testPalindromeLinkedList([3,4,4,3,6]) . PHP_EOL;
 
 // 5.
-$list5 = new SinglyLinkedList([3,6,4,4,3,6]);
-echo palindromeLinkedList($list5->head) . PHP_EOL;
+echo testPalindromeLinkedList([3,6,4,4,3,6]) . PHP_EOL;
 
 // 6.
-$list6 = new SinglyLinkedList([3,6,4,4,6,3]);
-echo palindromeLinkedList($list6->head) . PHP_EOL;
+echo testPalindromeLinkedList([3,6,4,4,6,3]) . PHP_EOL;
 
 // 7.
-$list7 = new SinglyLinkedList([1,2,3,2,1]);
-echo palindromeLinkedList($list7->head) . PHP_EOL;
+echo testPalindromeLinkedList([1,2,3,2,1]) . PHP_EOL;
