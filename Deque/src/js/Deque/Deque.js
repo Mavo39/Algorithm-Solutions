6f0b6a17bed0b78239bc5738dabb9bbc50cb976d
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-24 20:00:00 +0900

    Implement reusable Deque class in JavaScript with basic operations

---
+++ Deque/src/js/Deque/Deque.js
@@ -0,0 +1,82 @@
+const Node = require('./Node.js');
+
+class Deque {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    peekFront(){
+        if(this.head === null){
+            return null;
+        }
+
+        return this.head.data;
+    }
+
+    peekBack(){
+        if(this.tail === null){
+            return null;
+        }
+
+        return this.tail.data;
+    }
+
+    enqueueFront(data){
+        if(this.head === null){
+            this.head = new Item(data);
+            this.tail = this.head;
+        }
+        else {
+            let newHeadNode = new Item(data);
+            newHeadNode.next = this.head;
+            this.head.prev = newHeadNode;
+            this.head = newHeadNode;
+        }
+    }
+
+    enqueueBack(data){
+        if(this.tail === null){
+            this.tail = new Item(data);
+            this.head = this.tail;
+        }
+        else {
+            let newTailNode = new Item(data);
+            newTailNode.prev = this.tail;
+            this.tail.next = newTailNode;
+            this.tail = newTailNode;
+        }
+    }
+
+    dequeueFront(){
+        if(this.head === null){
+            return null;
+        }
+
+        const temp = this.head;
+        this.head = this.head.next;
+        if(this.head === null){
+            this.tail = null;
+            return null;
+        }
+
+        return temp.data;
+    }
+
+    dequeueBack(){
+        if(this.tail === null){
+            return null;
+        }
+
+        const temp = this.tail;
+        this.tail = this.tail.prev;
+        if(this.tail === null){
+            this.head = null;
+            return null;
+        }
+
+        return temp.data;
+    }
+}
+
+module.exports = Deque;
