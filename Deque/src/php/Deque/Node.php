fde695c73cc95326bf12307d421a9f73adbbcf79
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-06-25 10:43:49 +0900

    Create initial files and directory for solving Deque problem in PHP and Implement Node class in PHP

---
+++ Deque/src/php/Deque/Node.php
@@ -0,0 +1,15 @@
+<?php
+
+class Node 
+{
+    public $data;
+    public $prev;
+    public $next;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+        $this->prev = null;
+        $this->next = null;
+    }
+}
