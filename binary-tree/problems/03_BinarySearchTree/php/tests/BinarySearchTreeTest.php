206cc9f1dc49e6817c58f756c08c048517e588d7
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-07-07 22:03:10 +0900

    Update README with hypothesis testing results and insights for BinarySearchTree problem and refactor keyExists method to reuse search method

--- binary-tree/problems/03_BinarySearchTree/php/tests/BinarySearchTreeTest.php
+++ binary-tree/problems/03_BinarySearchTree/php/tests/BinarySearchTreeTest.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../../../../src/php/BinarySearchTree.php';
 $balancedBST = new BinarySearchTree([1,2,3,4,5,6,7,8,9,10,11]);
 
 // 1.
-echo $balancedBST->keyExists(6) === true ? "true" : "false" . PHP_EOL;
+echo ($balancedBST->keyExists(6) === true ? "true" : "false") . PHP_EOL;
 
 // 2.
 echo json_encode($balancedBST->search(6)) . PHP_EOL;
