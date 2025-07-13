7defbb324d8f3f9e387258bc68a412a992f80261
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-07-13 21:43:44 +0900

    WIP: Implement binarySubtreeToArray function to traslate binaryTree to array

---
+++ binary-tree/src/js/binaryTreeToArray.js
@@ -0,0 +1,13 @@
+function binarySubtreeToArray(root){
+    if(root === null){
+        return [];
+    }
+
+    const result = [];
+    const queue = [root];
+
+    while(queue.length > 0){
+        
+    }
+        
+}
