88745a14597dbf72bcefa2c3eeb352d5aece3691
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-07-24 20:48:04 +0900

    Implement successor function in PHP to reuse in successor function

--- binary-tree/problems/07_successor/php/src/successor.php
+++ binary-tree/problems/07_successor/php/src/successor.php
@@ -4,5 +4,23 @@ require_once __DIR__ . '/../../../../src/php/BinaryTree.php';
 
 function successor(?BinaryTree $root, int $key): ?BinaryTree
 {
-    
+
+}
+
+function findNode(?BinaryTree $root, int $key): ?BinaryTree
+{
+    $currentNode = $root;
+
+    while($currentNode !== null){
+        if($currentNode->data === $key){
+            return $currentNode;
+        }
+        if($currentNode->data < $key){
+            $currentNode = $currentNode->left;
+        } else {
+            $currentNode = $currentNode->right;
+        }
+    }
+
+    return $currentNode;
 }
