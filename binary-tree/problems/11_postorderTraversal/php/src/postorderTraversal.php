ed99e3d4d75c5f1ecaf3629cde014df272c9e436
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-08-05 12:51:34 +0900

    Implement postorderTraversal in PHP

---
+++ binary-tree/problems/11_postorderTraversal/php/src/postorderTraversal.php
@@ -0,0 +1,17 @@
+<?php 
+
+function postorderTraversal(?BinaryTree $root): array
+{
+    $resArr = [];
+    if($root === null) return $resArr;
+    return postorderTraversalHelper($root, $resArr);
+}
+
+function postorderTraversalHelper(?BinaryTree $node, array &$arr): array
+{
+    postorderTraversalHelper($node->left, $arr);
+    postorderTraversalHelper($node->right, $arr);
+    $arr[] = $node->data;
+
+    return $arr;
+}
