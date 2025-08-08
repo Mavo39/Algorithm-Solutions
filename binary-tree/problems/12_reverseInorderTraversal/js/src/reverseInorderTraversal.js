b98dfdbb3ab12640483304317097971adb092c9b
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-08-08 13:13:49 +0900

    Implement reverseInorderTraversal in JavaScript

---
+++ binary-tree/problems/12_reverseInorderTraversal/js/src/reverseInorderTraversal.js
@@ -0,0 +1,13 @@
+function reverseInorderTraversal(root){
+    return reverseInorderTraversalHelper(root, []);
+}
+
+function reverseInorderTraversalHelper(root, arr){
+    if(root === null) return arr;
+
+    reverseInorderTraversalHelper(root.right);
+    arr.push(root.data);
+    reverseInorderTraversalHelper(root.left);
+
+    return arr;
+}
