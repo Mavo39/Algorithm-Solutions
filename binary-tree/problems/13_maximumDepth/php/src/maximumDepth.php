f564f212f7446036eeb159b8cc41147aa41b5333
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-08-17 10:37:15 +0900

    refactor: remove redundant check for root-only tree

--- binary-tree/problems/13_maximumDepth/php/src/maximumDepth.php
+++ binary-tree/problems/13_maximumDepth/php/src/maximumDepth.php
@@ -3,8 +3,6 @@
 function maximumDepth(?BinaryTree $root): int
 {
     if($root === null) return 0;
-    if($root->left === null && $root->right === null) return 0;
-
     return maximumDepthHelper($root, 0);
 }
 
