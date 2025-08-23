ab24837baf586f50b90b27fd26adda2e317d4055
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-08-23 11:06:33 +0900

    Implement allElementsSorted in PHP

--- binary-tree/problems/15_allElementsSorted/php/src/allElementsSorted.php
+++ binary-tree/problems/15_allElementsSorted/php/src/allElementsSorted.php
@@ -2,7 +2,33 @@
 
 function allElememtsSorted(?BinaryTree $root1, ?BinaryTree $root2): array
 {
-    
+    $arr1 = [];
+    $arr2 = [];
+
+    $arr1 = inOrderWalk($root1, $arr1);
+    $arr2 = inOrderWalk($root2, $arr2);
+
+    $resArr = [];
+    $arr1Index = 0;
+    $arr2Index = 0;
+
+    while($arr1Index < count($arr1) && $arr2Index < count($arr2)){
+        if($arr1[$arr1Index] < $arr2[$arr2Index]){
+            $resArr[] = $arr1[$arr1Index++];
+        } else {
+            $resArr[] = $arr2[$arr2Index++];
+        }
+    }
+
+    while($arr1Index < count($arr1)){
+        $resArr[] = $arr1[$arr1Index++];
+    }
+
+    while($arr2Index < count($arr2)){
+        $resArr[] = $arr2[$arr2Index++];
+    }
+
+    return $resArr;
 }
 
 function inOrderWalk(?BinaryTree $root, array $arr): array
