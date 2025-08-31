167606c25a02b38a1d514bada18ceb440ab20abc
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-08-31 11:59:35 +0900

    feat: Implement minDepth in JavaScript

--- binary-tree/problems/16_minDepth/src/minDepth.js
+++ binary-tree/problems/16_minDepth/src/minDepth.js
@@ -10,7 +10,12 @@ function minDepth(root){
         const length = queue.length;
         for(let i = 1; i <= length; i++){
             currentNode = queue.shift();
-
+            if(currentNode.left === null && currentNode.right == null) return level;
+            if(currentNode.left != null) queue.push(currentNode.left);
+            if(currentNode.right != null) queue.push(currentNode.right);
+            level++;
         }
     }
+
+    return level;
 }
