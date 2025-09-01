3d416f213b34f44b873a6a99b73c64cda0618db6
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-09-01 16:38:50 +0900

    feat: Implement test cases for minDepth in JavaScript

--- binary-tree/problems/16_minDepth/js/tests/minDepthTest.js
+++ binary-tree/problems/16_minDepth/js/tests/minDepthTest.js
@@ -1,12 +1,41 @@
+const minDepth = require('../src/minDepth.js');
+const toBinaryTree = require('../../../../src/js/toBinaryTree.js');
 
+const tests = {
+    case1 : {
+        'input' : [8,9,11], 
+        'output' : 1
+    }, 
+    case2 : {
+        'input' : [93,16,65,80],
+        'output' : 1
+    }, 
+    case3 : {
+        'input' : [92,88,53,36,27,16,80,45],
+        'output' : 2
+    }, 
+    case4 : {
+        'input' : [44,12,49,3,29,46,62,null,null,null,null,null,null,null,70],
+        'output' : 2
+    }, 
+    case5 : {
+        'input' : [19,34,73,39,56,4,86,17,84,34],
+        'output' : 2
+    }, 
+    case6 : {
+        'input' : [97,10,77,32,40,70,32,96,27,10,12,93,82,33,55,71,59,82,37,75,25,31,14,96,85,41,28,70,9,56,8,90,8,65,49,45,34,30,25,7,7,97,23,66,84,57,38,38,95,9,77,60,44,3,81,41,89,90,73,100,86,53,96,40],
+        'output' : 5
+    }, 
+    case7 : {
+        'input' : [null],
+        'output' : 0
+    }
+};
 
+for(let [key, test] of Object.entries(tests)){
+    const root = toBinaryTree(test['input']);
+    const res = minDepth(root);
+    const resCheck = (res === test['output']) ? "True" : "False";
 
-// | ケース | 入力 | 出力 |
-// | - | - | - |
-// | 1 | `toBinaryTree([8,9,11])` | `1` |
-// | 2 | `toBinaryTree([93,16,65,80])` | `1` |
-// | 3 | `toBinaryTree([92,88,53,36,27,16,80,45])` | `2` |
-// | 4 | `toBinaryTree([44,12,49,3,29,46,62,null,null,null,null,null,null,null,70])` | `2` |
-// | 5 | `toBinaryTree([19,34,73,39,56,4,86,17,84,34])` | `2` |
-// | 6 | `toBinaryTree([97,10,77,32,40,70,32,96,27,10,12,93,82,33,55,71,59,82,37,75,25,31,14,96,85,41,28,70,9,56,8,90,8,65,49,45,34,30,25,7,7,97,23,66,84,57,38,38,95,9,77,60,44,3,81,41,89,90,73,100,86,53,96,40])` | `5` |
-// | 7 | `toBinaryTree([null])` | `0` |
+    console.log(`Test ${key}: ${resCheck}`);
+}
