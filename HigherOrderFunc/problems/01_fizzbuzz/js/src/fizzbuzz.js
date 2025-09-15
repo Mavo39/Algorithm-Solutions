991106d29ea98822b6ca821ab17b4e908fcb30e3
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-09-15 20:38:02 +0900

    refactor: Generate string without leading hyphen by using .slice()

--- HigherOrderFunc/problems/01_fizzbuzz/js/src/fizzbuzz.js
+++ HigherOrderFunc/problems/01_fizzbuzz/js/src/fizzbuzz.js
@@ -1,15 +1,14 @@
 let fizzbuzz = n => {
-    if(n === 0) return "";
-    
     let resStr = "";
-    for(let i = 0; i < n; i++){
+
+    for(let i = 1; i <= n; i++){
         if(i % 15 === 0) resStr += "-FizzBuzz";
         else if(i % 5 === 0) resStr += "-Buzz";
         else if(i % 3 === 0) resStr += "-Fizz";
         else resStr += `-${i}`;
     }
 
-    return resStr;
+    return resStr.slice(1);
 }
 
 module.exports = fizzbuzz;
