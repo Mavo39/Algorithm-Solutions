ac1ed7cf73a2da2393a853aefc1e46d5117a6cd8
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-10-13 20:10:59 +0900

    feat: Implement federalAndStateTax in JavaScript

---
+++ HigherOrderFunc/problems/03_federalAndStateTax/js/src/federalAndStateTax.js
@@ -0,0 +1,19 @@
+const stateTaxRate = {
+    "Arizona" : 0.049,
+    "California" : 0.088,
+    "Northcarolina" : 0.025
+};
+
+let getFederalTaxAmount = income => income * 0.21;
+
+let getStateTaxAmount = (income, state) => income * stateTaxRate[state];
+
+let getTotalTaxAmount = (income, state) => {
+    const federalTaxAmount = getFederalTaxAmount(income);
+    const stateTaxAmount = getStateTaxAmount(income, state);
+    return federalTaxAmount + stateTaxAmount;
+};
+
+let printTaxByState = income => state => {
+    console.log(`Federal Tax: ${getFederalTaxAmount(income)}\n${state} State Tax: ${getStateTaxAmount(income, state)}\nTax Amount: ${getTotalTaxAmount(income, state)}`);
+}
