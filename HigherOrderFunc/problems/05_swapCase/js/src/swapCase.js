0e83c790c983de7af5777a5e7894482435f8fefa
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-10-21 07:59:38 +0900

    feat: Implement swapCase problem in JS

---
+++ HigherOrderFunc/problems/05_swapCase/js/src/swapCase.js
@@ -0,0 +1,6 @@
+function swapCase(charList){
+        return charList.map(char => {
+        if((/[a-z]/).test(char)) return char.toUpperCase();
+        else if(/[A-Z]/.test(char)) return char.toLowerCase();
+    });
+}
