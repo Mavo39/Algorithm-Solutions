92b9c022322110924d7f9e4764640eb85e324fd4
Masaya Sato <140931393+Mavo39@users.noreply.github.com>
2025-10-27 10:26:58 +0900

    refactor: change calculate logic to calculate first and second argument in array

--- HigherOrderFunc/problems/06_calcDistanceList/js/src/calcDistance.js
+++
@@ -1,3 +0,0 @@
-function calcDistanceList(pointList){
-    return pointList.map(point => Math.abs(parseInt(point)));
-}
---
+++ HigherOrderFunc/problems/06_calcDistanceList/js/src/calcDistanceList.js
@@ -0,0 +1,7 @@
+function calcDistanceList(pointList){
+    return pointList.map(str => {
+        const strArr = str.split("-");
+        const res = Math.abs(strArr[0] - strArr[1]);
+        return res;
+    });
+}
